c0ec81650f12d3e9ab9bc7b73d015c39daf8e153
lindi-imre <lindi.imre@gmail.com>
2016-01-18 11:09:29 +0100

    final
    final

--- 03_aggregate_functions_and_group_by.sql
+++ 03_aggregate_functions_and_group_by.sql
@@ -1,11 +1,6 @@
-#Write a query to get the number of employees with the same job.
-SELECT job_id, COUNT(job_id) AS count FROM employees GROUP BY job_id
-#Write a query to find the manager ID and the salary of the lowest-paid employee for that manager.
-SELECT job_id, MIN(salary) FROM employees WHERE job_id LIKE '%MGR%' OR job_id LIKE '%MAN%'
-#Write a query to get the department ID and the total salary payable in each department.
-SELECT job_id, count(salary) AS num_of_employees FROM employees GROUP BY job_id
-#Write a query to get the average salary for each job ID excluding programmer.
-SELECT job_id, AVG(salary) AS avg_salary FROM employees where job_id != 'IT_PROG' GROUP BY job_id
-
-SELECT job_id, max_salary FROM jobs where max_salary >= 4000
-SELECT job_id, AVG(SALARY) FROM employees GROUP BY job_id HAVING COUNT(job_id) > 10
+SELECT job_id, COUNT(job_id) AS count FROM employees GROUP BY job_id;
+SELECT job_id, MIN(salary) FROM employees WHERE job_id LIKE '%MGR%' OR job_id LIKE '%MAN%';
+SELECT job_id, count(salary) AS num_of_employees FROM employees GROUP BY job_id;
+SELECT job_id, AVG(salary) AS avg_salary FROM employees where job_id != 'IT_PROG' GROUP BY job_id;
+SELECT job_id, max_salary FROM jobs where max_salary >= 4000;
+SELECT job_id, AVG(SALARY) FROM employees GROUP BY job_id HAVING COUNT(job_id) > 10;
--- 05_joins.sql
+++ 05_joins.sql
@@ -1,10 +1,5 @@
-"""Write a query to find the addresses (location_id, street_address, city, state_province, country_name) of all the departments."""
-SELECT location_id, street_address, city, state_province, country_id FROM locations NATURAL JOIN departments
-"""Write a query to find the names (first_name, last name), department ID and name of all the employees."""
-SELECT emp.first_name, emp.last_name, emp.department_id, dep.department_name from employees as emp INNER JOIN departments as dep on dep.department_id = emp.department_id
-"""#Find the names (first_name, last_name), job, department number, and department name of the employees who work in London."""
-SELECT emp.first_name, emp.last_name, emp.department_id, jobs.job_title, loc.city from employees as emp INNER JOIN jobs on emp.job_id = jobs.job_id inner join departments as dep on emp.department_id = dep.department_id inner join locations as loc on loc.location_id = dep.location_id where loc.city = 'London'
-"""Write a query to get the department name and number of employees in the department."""
-SELECT department_name, count(department_name) FROM departments natural join employees as emp group by emp.department_id
-
-select first_name, last_name, hire_date from employees where hire_date > (SELECT (SELECT hire_date FROM employees where last_name = 'Jones'))
+SELECT location_id, street_address, city, state_province, country_id FROM locations NATURAL JOIN departments;
+SELECT emp.first_name, emp.last_name, emp.department_id, dep.department_name from employees as emp INNER JOIN departments as dep on dep.department_id = emp.department_id;
+SELECT emp.first_name, emp.last_name, emp.department_id, jobs.job_title, loc.city from employees as emp INNER JOIN jobs on emp.job_id = jobs.job_id inner join departments as dep on emp.department_id = dep.department_id inner join locations as loc on loc.location_id = dep.location_id where loc.city = 'London';
+SELECT department_name, count(department_name) FROM departments natural join employees as emp group by emp.department_id;
+select first_name, last_name, hire_date from employees where hire_date > (SELECT (SELECT hire_date FROM employees where last_name = 'Jones'));
